330fbdf34a7959b94a8a688a10b4a24386d1bfa2
KmiloLopez <andres.lopez2018@outlook.es>
2024-08-16 13:40:20 -0500

    3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Wordle-React-JS/",
+  base: "https://kmilolopez.github.io/Wordle-React-JS",
 });
